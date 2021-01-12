acadbc5ff823bd5c4ff68abbb6f865d8c5c172d7
Lucas <lucasbarbosa@outlook.com>
2021-01-12 19:38:30 -0300

    first commit

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,15 @@
 import React from 'react'
-
 import Result from './Result'
 
 function Results ({ results, openPopup }) {
 	return (
 		<section className="results">
 			{results.map(result => (
-				<Result key={result.imdbID} result={result} openPopup={openPopup} />
+				<Result
+					key={result.imdbID}
+					result={result}
+					openPopup={openPopup}
+				/>
 			))}
 		</section>
 	)
